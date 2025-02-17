66bc2eeaeb8b548a7d293f1ee70838205fe2bb60
Roman <60399380+cd-roman@users.noreply.github.com>
2025-02-18 00:12:39 +0100

    Update project configuration and dependencies, update index.css and App.css, add SVG support with vite-plugin-svgr

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module '*.svg' {
+    import * as React from 'react';
+  
+    const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  
+    export {
+      ReactComponent
+    }
+    
+    const src: string;
+    export default src;
+  }
+  
