367259cfd7f79e55e1d9cb1ae47628b083e91a33
Roman <60399380+cd-roman@users.noreply.github.com>
2025-02-18 17:33:04 +0100

    Add Airport and Flights types; create popular-flights data file; update Layout component to use global context; fix bug with CustomSwitch

--- src/components/modules/Layout/Layout.tsx
+++ src/components/modules/Layout/Layout.tsx
@@ -1,67 +1,69 @@
-import {ReactNode, useState} from "react";
-import {Link} from "react-router-dom";
-import {Grid, Stack, useMediaQuery} from "@mui/material";
-import {CustomSwitch, Hamburger, Navigation} from "../../base";
-import styles from './Layout.module.scss'
-import {routesPath} from "../../../routes";
-
+import { ReactNode, useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { Grid, Stack, useMediaQuery } from "@mui/material";
+import {
+  CustomSwitch,
+  Hamburger,
+  MyGlobalContext,
+  Navigation,
+} from "../../base";
+import styles from "./Layout.module.scss";
+import { routesPath } from "../../../routes";
 
 interface LayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-export const Layout = ({children}: LayoutProps) => {
-    const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
-    const isMediumDevice = useMediaQuery('(max-width:900px)');
-  
-    const openMenu = () => {
-      setIsOpenMenu(isOpen => !isOpen);
-    }
+export const Layout = ({ children }: LayoutProps) => {
+  const { length, weight, setLength, setWeight } = useContext(MyGlobalContext);
+  const [checkLength, setCheckLength] = useState(length === "km");
+  const [checkWeight, setCheckWeight] = useState(weight === "kg");
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+  const isMediumDevice = useMediaQuery("(max-width:900px)");
 
+  const openMenu = () => {
+    setIsOpenMenu((isOpen) => !isOpen);
+  };
 
   return (
-    <Grid
-      height={'100vh'}
-      container
-      className={styles.layout}
-    >
-      <Grid item md='auto' className={styles.sidebar}>
+    <Grid height={"100vh"} container className={styles.layout}>
+      <Grid item md="auto" className={styles.sidebar}>
         <Stack
           className={styles.navigation}
-          direction={{xs: 'row', md: 'column' }}
+          direction={{ xs: "row", md: "column" }}
         >
           <div>
             <Link className={styles.logo} to={routesPath.home}>
-                <img src="/logo.png" width='100%' alt="Home page"/>
+              <img src="/logo.png" width="100%" alt="Home page" />
             </Link>
           </div>
           <Navigation isOpenMenu={isOpenMenu} />
           <div className={styles.switches}>
-
             <CustomSwitch
-              label1={'mi'}
-              label2={'km'}
+              label1={"mi"}
+              label2={"km"}
+              value={checkLength}
+              setGlobalValue={setLength}
+              setValue={setCheckLength}
               type={length}
             />
 
             <CustomSwitch
-              label1={'lb'}
-              label2={'kg'}
+              label1={"lb"}
+              label2={"kg"}
+              value={checkWeight}
+              setGlobalValue={setWeight}
+              setValue={setCheckWeight}
+              type={weight}
             />
           </div>
-          {isMediumDevice && (
-            <Hamburger onSetActiveMenu={openMenu} />
-          )}
+          {isMediumDevice && <Hamburger onSetActiveMenu={openMenu} />}
         </Stack>
       </Grid>
 
-      <Grid item md className={styles.content} sx={{height: '100%'}}>
-        <Stack
-          className={styles.board}
-        >
-            {children}
-        </Stack>
+      <Grid item md className={styles.content} sx={{ height: "100%" }}>
+        <Stack className={styles.board}>{children}</Stack>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,36 @@ export type RouteType = {
     name: string;
 };
 
+export type Airport = {
+  id: number;
+  ident: string;
+  type: string;
+  name: string;
+  latitude_deg: number;
+  longitude_deg: number;
+  elevation_ft: number;
+  continent: string;
+  iso_country: string;
+  iso_region: string;
+  municipality: string;
+  scheduled_service: string;
+  gps_code: string;
+  iata_code: string;
+  local_code: string;
+  home_link: string;
+  wikipedia_link: string;
+  keywords: string;
+};
+
+export type Flights = {
+  id: string;
+  departure: string;
+  arrival: string;
+  distance: string;
+  carbon: string;
+  custom?: boolean;
+};
+
 export type Cars = {
   id: number;
   type: string;
